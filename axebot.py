91b46f5539e1111ba5ace9096961d5e594d9bcf3
LUCIANO DINIZ GUERRA SANTIAGO <32240427+lucianodgs@users.noreply.github.com>
2018-10-17 19:54:07 -0300

    Leitura json e bitfinex concluida

--- axebot.py
+++ axebot.py
@@ -1,17 +1,40 @@
+## Código de exemplo API Bitfinex.
+##Author: Luciano Diniz Guerra Santiago
+##Date: 17/10/2018
+
+#symbols = client.symbols()
+#print(symbols)
+#print(client.ticker(symbol))
+#print(client.lendbook('btc', parameters))
+#print(client.order_book(symbol, parameters))
+#print(client.stats(symbol))
+##
+
+
+import json
+import time
 from bitfinex.client import Client
 
 client = Client()
 
-symbols = client.symbols()
-#print(symbols)
+def createTimeStamp(datestr, format="%Y-%m-%d %H:%M:%S"):
+    return time.mktime(time.strptime(datestr, format))
+
+
 
 symbol = 'btgusd'
 
-#print(client.ticker(symbol))
-print(client.today(symbol))
-print(client.stats(symbol))
+
+ret = client.today(symbol)
+ret['low']
+ret['high']
+ret['volume']
+
+
+
+#print(client.today(symbol))
+
+print(client.ticker(symbol))
 
 parameters = {'limit_asks': 2, 'limit_bids': 2}
 
-#print(client.lendbook('btc', parameters))
-#print(client.order_book(symbol, parameters))
