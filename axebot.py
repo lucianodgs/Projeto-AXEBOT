c9fb7a80ad6e3ec5347451fc696df34728d88e1e
LUCIANO DINIZ GUERRA SANTIAGO <32240427+lucianodgs@users.noreply.github.com>
2018-10-18 17:53:42 -0300

    Pegando o valor do ticket

--- axebot.py
+++ axebot.py
@@ -4,37 +4,31 @@
 
 #symbols = client.symbols()
 #print(symbols)
+#ret = client.today(symbol)
 #print(client.ticker(symbol))
 #print(client.lendbook('btc', parameters))
 #print(client.order_book(symbol, parameters))
 #print(client.stats(symbol))
-##
-
+#parameters = {'limit_asks': 2, 'limit_bids': 2}
+# ret['bid'] --Ultima compra
+# ret['ask'] --Ultima venda
+#
 
 import json
 import time
 from bitfinex.client import Client
 
+period = 10
+symbol = 'xrpusd'
 client = Client()
 
 def createTimeStamp(datestr, format="%Y-%m-%d %H:%M:%S"):
     return time.mktime(time.strptime(datestr, format))
 
 
+while True:
+    ret_day = client.today(symbol)
+    ret = client.ticker(symbol)
 
-symbol = 'btgusd'
-
-
-ret = client.today(symbol)
-ret['low']
-ret['high']
-ret['volume']
-
-
-
-#print(client.today(symbol))
-
-print(client.ticker(symbol))
-
-parameters = {'limit_asks': 2, 'limit_bids': 2}
-
+    print("Compra: %s ;Venda: %s ; Menor: %s ;Maior: %s; Data: %s" %(ret['bid'],ret['ask'],  ret_day['low'], ret_day['high'], time.ctime(ret['timestamp'])))
+    time.sleep(int(period))
