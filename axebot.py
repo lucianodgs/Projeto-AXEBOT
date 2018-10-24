116c3e623747904e1ba92c7f11ccee1663684bfd
LUCIANO DINIZ GUERRA SANTIAGO <32240427+lucianodgs@users.noreply.github.com>
2018-10-24 19:09:44 -0300

    PEP8 coding

--- axebot.py
+++ axebot.py
@@ -1,29 +1,28 @@
-## Código de exemplo API Bitfinex.
-##Author: Luciano Diniz Guerra Santiago
-##Date: 17/10/2018
+# Código de exemplo API Bitfinex.
+# Author: Luciano Diniz Guerra Santiago
+# Date: 17/10/2018
 
-#symbols = client.symbols()
-#print(symbols)
-#ret = client.today(symbol)
-#print(client.ticker(symbol))
-#print(client.lendbook('btc', parameters))
-#print(client.order_book(symbol, parameters))
-#print(client.stats(symbol))
-#parameters = {'limit_asks': 2, 'limit_bids': 2}
-# ret['bid'] --Ultima compra
-# ret['ask'] --Ultima venda
-#
+# symbols = client.symbols()
+# print(symbols)
+# ret = client.today(symbol)
+# print(client.ticker(symbol))
+# print(client.lendbook('btc', parameters))
+# print(client.order_book(symbol, parameters))
+# print(client.stats(symbol))
+# parameters = {'limit_asks': 2, 'limit_bids': 2}
+# ret['bid'] Ultima compra
+# ret['ask'] Ultima venda
 
-import json
+
 import time
 from bitfinex.client import Client
 
 period = 1
 symbol = 'xrpusd'
 client = Client()
-tendencia = ['Subir','Cair']
+tendencia = ['Alta', 'Baixa']
 id_tendencia = 0
-ultimas_compras = [0.0,0.0,0.0]
+ultimas_compras = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 cont_compras = 0
 
 
@@ -32,12 +31,12 @@ while True:
         ret_day = client.today(symbol)
         ret = client.ticker(symbol)
     except Exception as inst:
-        print("erro: %s" %inst)
+        print("Erro: %s" % inst)
         continue
 
     if ultimas_compras[cont_compras] == 0:
         ultimas_compras[cont_compras] = ret['bid']
-        id_tendencia=0
+        id_tendencia = 0
     elif ultimas_compras[cont_compras] <= ret['bid']:
         ultimas_compras[cont_compras] = ret['bid']
         id_tendencia = 0
@@ -45,11 +44,21 @@ while True:
         ultimas_compras[cont_compras] = ret['bid']
         id_tendencia = 1
 
-    if cont_compras == 2:
+    if cont_compras == 5:
         cont_compras = 0
     else:
-        cont_compras+=1
+        cont_compras += 1
 
-    print("%s | %s | %s" %(ultimas_compras[0],ultimas_compras[1],ultimas_compras[2]))
-    print("Compra: %s ;Venda: %s ; Menor: %s ;Maior: %s; Data: %s | Tendencia: %s"  %(ret['bid'],ret['ask'],  ret_day['low'], ret_day['high'], time.ctime(ret['timestamp']),tendencia[id_tendencia]))
+    print("%s | %s | %s"
+          % (ultimas_compras[0],
+             ultimas_compras[1],
+             ultimas_compras[2]))
+    print("Compra: %s ;Venda: %s ; Menor: %s ;Maior: %s; Data: %s | Tendencia: %s"
+          % (ret['bid'],
+             ret['ask'],
+             ret_day['low'],
+             ret_day['high'],
+             time.ctime(ret['timestamp']),
+             tendencia[id_tendencia]))
+
     time.sleep(int(period))
